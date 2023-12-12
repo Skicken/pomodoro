4275818ace8b6c5792c152de9e0d54dd55101bc6
Skicken <45664464+Skicken@users.noreply.github.com>
2023-12-12 16:36:23 +0100

    Initial commit

---
+++ apps/pomodoro/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="pomodoro" />
+    </div>
+  );
+}
+
+export default App;
